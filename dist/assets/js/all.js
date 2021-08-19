6aaf4e78c76715402dc94f36992dc3bafcdd96b3
paisley <paisley.chien@stantec.com>
2021-08-19 12:16:49 +0800

    facebook icon fix and font import

--- dist/assets/js/all.js
+++ dist/assets/js/all.js
@@ -1,9 +1,4 @@
 "use strict";
-
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
-});
 "use strict";
 
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) { symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); } keys.push.apply(keys, symbols); } return keys; }
